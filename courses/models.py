5fe8bad392205902db96d6b1561d0a287a94ae4a
Nick Andreev <bakasaru@list.ru>
2019-11-24 14:15:36 +0300

    Миграции

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Organizer(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name='Название')
+    logo = models.CharField(max_length=255, verbose_name='Логотип')
+    url = models.CharField(max_length=255, verbose_name='Ссылка')
+
+    class Meta:
+        verbose_name = 'Организатор'
+        verbose_name_plural = 'Организаторы'
+
+
+class Group(models.Model):
+    id = models.IntegerField(primary_key=True)
+    code = models.CharField(max_length=8, verbose_name='Код')
+    name = models.CharField(max_length=255, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Направление подготовки'
+        verbose_name_plural = 'Направления подготовки'
+
+
+class Course(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name='Название')
+    url = models.CharField(max_length=255, verbose_name='Ссылка')
+    groups = models.ManyToManyField(Group, verbose_name='Направления подготовки')
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE, verbose_name='Организатор')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
